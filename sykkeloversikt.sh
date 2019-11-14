8eac318f1f7adcb762189dae8c2b9060ba98c23e
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2019-11-14 21:02:00 +0100

    Loop stations array and build up result json: with name, bikes and docks

--- sykkeloversikt.sh
+++ sykkeloversikt.sh
@@ -7,3 +7,13 @@ STATIONS=$(curl -s https://gbfs.urbansharing.com/oslobysykkel.no/station_informa
 STATUS=$(curl -s https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json | jq -c '.data.stations[]')
 
 readarray -t station_array <<< "${STATIONS}"
+
+for station in "${station_array[@]}"; do
+  station_id=$(jq '. | .station_id' <<< "${station}")
+  station_name=$(jq '. | .name' <<< "${station}")
+  station_status=$(jq -c ". | select(.station_id==${station_id})" <<< "${STATUS}")
+  bikes=$(jq '. | .num_bikes_available' <<< "${station_status}")
+  docks=$(jq '. | .num_docks_available' <<< "${station_status}")
+
+  result=${result}"{\"name\": $station_name, \"bikes\": $bikes, \"docks\": $docks},"
+done
