72b4894bb8501a67af4f385991a8a906fef0ba3e
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2019-11-14 20:53:47 +0100

    Add stations to a bash array

--- sykkeloversikt.sh
+++ sykkeloversikt.sh
@@ -5,3 +5,5 @@ command -v jq >/dev/null 2>&1 || { echo >&2 "Required progam jq not installed ru
 
 STATIONS=$(curl -s https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json | jq -c '.data.stations[]') 
 STATUS=$(curl -s https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json | jq -c '.data.stations[]')
+
+readarray -t station_array <<< "${STATIONS}"
