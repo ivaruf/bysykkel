46f9d5ff489b3f56b7a1e12d546f904b033faf0f
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2019-11-14 20:51:12 +0100

    Read stations and status data into variables
    This syntax requires bash 4.2, might not work on a mac also.

--- sykkeloversikt.sh
+++ sykkeloversikt.sh
@@ -2,3 +2,6 @@
 
 # Check if jq is installed, if not quit
 command -v jq >/dev/null 2>&1 || { echo >&2 "Required progam jq not installed run: \"sudo apt-get install jq\""; exit 1; }
+
+STATIONS=$(curl -s https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json | jq -c '.data.stations[]') 
+STATUS=$(curl -s https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json | jq -c '.data.stations[]')
