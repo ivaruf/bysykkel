61b76e49863a0d25383174e114c1d5650e006c61
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2019-11-25 23:05:14 +0100

    Add v2 of bysykkel api adding lat and lon for stations
    This is meant as an example of how (easily) the api can be updated
    with new versions.

--- sykkeloversikt.sh
+++ sykkeloversikt.sh
@@ -11,10 +11,17 @@ readarray -t station_array <<< "${STATIONS}"
 for station in "${station_array[@]}"; do
   station_id=$(jq '. | .station_id' <<< "${station}")
   station_name=$(jq '. | .name' <<< "${station}")
+  station_lat=$(jq '. | .lat' <<< "${station}")
+  station_lon=$(jq '. | .lon' <<< "${station}")
   station_status=$(jq -c ". | select(.station_id==${station_id})" <<< "${STATUS}")
   bikes=$(jq '. | .num_bikes_available' <<< "${station_status}")
   docks=$(jq '. | .num_docks_available' <<< "${station_status}")
 
-  result=${result}"{\"name\": $station_name, \"bikes\": $bikes, \"docks\": $docks},"
+  v1=${v1}"{\"name\": $station_name, \"bikes\": $bikes, \"docks\": $docks},"
+  v2=${v2}"{\"name\": $station_name, \"bikes\": $bikes, \"docks\": $docks, \"lat\": $station_lat, \"lon\": $station_lon},"
 done
-jq . <<< [${result%?}]
+jq . <<< [${v1%?}] > v1_out.json
+jq . <<< [${v2%?}] > v2_out.json
+
+# Print v1 json to ensure same output as v1 (original task)
+jq . v1_out.json
--- updater.sh
+++ updater.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -e
 
-VERSION=api/bysykkel/v1/
+V_1=api/bysykkel/v1/
+V_2=api/bysykkel/v2/
 
-# Make sure path exsists
-mkdir -p "${VERSION}"
+# Make sure paths exsists
+mkdir -p "${V_1}"
+mkdir -p "${V_2}"
 
-echo "Updating data for ${VERSION} Press [CTRL+C] to stop."
+echo "Updating data for api Press [CTRL+C] to stop."
 
 while :
 do
   printf "Data is updating ..."
-  ./sykkeloversikt.sh > tmp.json
-  mv tmp.json ${VERSION}/oversikt.json
+  ./sykkeloversikt.sh > /dev/null 2>&1
+  mv v1_out.json ${V_1}/oversikt.json
+  mv v2_out.json ${V_2}/oversikt.json
   printf " Done: "
   date
   sleep 10
